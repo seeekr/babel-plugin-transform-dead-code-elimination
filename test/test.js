fa3b59500d7a0383200610c829dbe280be1cdc22
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-03-07 16:36:23 -0500

    test harness normalizes whitespace

--- test/test.js
+++ test/test.js
@@ -1,33 +1,38 @@
 import test from 'ava';
 import { transformFile } from 'babel-core';
 import fs from 'mz/fs';
-import { basename, join } from 'path';
+import path from 'path';
 import globby from 'globby';
 
 async function transform(name) {
-	const optionsPath = join(__dirname, 'actual', `${name}.json`);
+	const optionsPath = path.join(__dirname, 'actual', `${name}.json`);
 	const hasOptions = await fs.exists(optionsPath);
 	const options = hasOptions ? require(optionsPath) : {};
 
 	options.plugins = options.plugins || [];
-	options.plugins.push(join(__dirname, '../lib/index.js'));
+	options.plugins.push(path.join(__dirname, '../lib/index.js'));
 	options.babelrc = false;
 
 	return new Promise((resolve, reject) => {
-		transformFile(join(__dirname, 'actual', `${name}.js`), options, (err, result) => {
+		transformFile(path.join(__dirname, 'actual', `${name}.js`), options, (err, result) => {
 			err ? reject(err) : resolve(result.code);
 		});
 	});
 }
 
-globby.sync(join(__dirname, 'actual/*.js')).forEach(source => {
-	const name = basename(source, '.js');
-	test(name, async t => {
-		const [expected, transformed] = await Promise.all([
-			fs.readFile(join(__dirname, 'expected', `${name}.js`), 'utf8'),
-			transform(name)
-		]);
+function normalize(string) {
+	// replace multiple newlines with one, normalize to unix line endings
+	return string.trim().replace(/[\r\n]+/g, '\n');
+}
 
-		t.is(expected.trim(), transformed.trim());
+globby.sync(path.join(__dirname, 'actual/*.js')).forEach(source => {
+	const name = path.basename(path.resolve(__dirname, 'actual', source), '.js');
+	test(name, async t => {
+		let [expected, transformed] = (await Promise.all([
+			fs.readFile(path.join(__dirname, 'expected', `${name}.js`), 'utf8'),
+			transform(name)
+		])).map(normalize);
+
+		t.is(expected, transformed);
 	});
 });
