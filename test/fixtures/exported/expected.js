3eded5ef0194d416f63ba8e770242d4e9a97ec0f
Erik <erikdesjardins@users.noreply.github.com>
2016-03-07 19:33:19 -0500

    transform exports to commonjs for that last bit of coverage

--- test/fixtures/exported/expected.js
+++ test/fixtures/exported/expected.js
@@ -1,6 +1,14 @@
-export default 0;
-export const foo = 1;
-export let bar = 2;
-export var baz = 3;
-export class Foo {}
-export function foobar() {}
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.foobar = foobar;
+exports.default = 0;
+const foo = exports.foo = 1;
+let bar = exports.bar = 2;
+var baz = exports.baz = 3;
+class Foo {}
+exports.Foo = Foo;
+function foobar() {}
